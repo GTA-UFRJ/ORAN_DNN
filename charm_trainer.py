d5e708975479b228ed3b61b560106948168efd91
pachecobeto95 <robertopvc@gmail.com>
2023-11-06 13:19:40 -0300

    a

--- charm_trainer.py
+++ charm_trainer.py
@@ -286,7 +286,7 @@ def charm_trainer(model_name="cnn", id_gpu="0", data_folder="./oran_dataset",
     modelPath="./models", resultPath="./results", n_epochs=25, batch_size=512, 
     chunk_size=20000, sample_stride=0, loaders=6, dg_coverage=0.75, tensorboard=None):
     
-    ct = CharmTrainer(id_gpu=id_gpu, data_folder=data_folder, modelPath=modelPath, resultPath=resultPath, 
+    ct = CharmTrainer(model_name=model_name, id_gpu=id_gpu, data_folder=data_folder, modelPath=modelPath, resultPath=resultPath, 
         batch_size=batch_size, chunk_size=chunk_size, sample_stride=sample_stride,
         loaders=loaders, dg_coverage=dg_coverage, tensorboard=tensorboard)
     
