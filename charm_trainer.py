598a34bbe910380dfc0b8286b6cb5ec09786af99
pachecobeto95 <robertopvc@gmail.com>
2023-11-09 17:08:00 -0300

    a

--- charm_trainer.py
+++ charm_trainer.py
@@ -162,7 +162,7 @@ class CharmTrainer(object):
             self.model = cnn_model.ConvModel().to(self.device)
 
         elif(self.model_name == "lstm"):
-            self.model = lstm.SequenceModel().to(self.device)
+            self.model = lstm.SequenceModel(n_features=2, n_classes=3).to(self.device)
 
         elif(self.model_name == "conv_rn"):
             self.model = conv_rn.ConvRNN().to(self.device)
