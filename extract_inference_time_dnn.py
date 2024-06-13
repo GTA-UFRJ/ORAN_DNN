869de5f62ba694f7f1eea25fab770038fc6c68e8
Roberto Pacheco <robertopvc@gmail.com>
2024-06-13 14:20:33 -0300

    Add files via upload

--- extract_inference_time_dnn.py
+++ extract_inference_time_dnn.py
@@ -39,9 +39,6 @@ def main(args):
 	DIR_PATH = os.path.dirname(__file__)
 
 	device = torch.device('cuda' if (torch.cuda.is_available() and args.use_gpu) else 'cpu')
-	print(device)
-	print(args.use_gpu)
-	sys.exit()
 		
 	inf_data_dir_path = os.path.join(DIR_PATH, "inf_data_results")
 	os.makedirs(inf_data_dir_path, exist_ok=True)
